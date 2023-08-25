a2c0083544f2c95669bcdbfbb1669d4884c24a19
ElbiGans04 <rhafaelbijaksana04@gmail.com>
2023-08-25 12:44:03 +0700

    feat: integrasi API
    melakukan integrasi API, membuat fitur pencarian, dan merubah
    design aplikasi

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,35 @@
-import { useState } from "react";
-import { useForm } from "react-hook-form";
-// import useSWR from "swr";
+import { useEffect, useMemo, useState } from "react";
+import fetcher from "./utils/fetcher";
+import useSWR from "swr";
 
 function App() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
-  const [filter, setFilter] = useState(false);
-  // const {data} = useSWR("https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1&query=Harry+Potter", fetcher);
-  // const { data } = useSWR(
-  //   "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1",
-  //   fetcher
-  // );
-  // console.log(data);
+  const [search, setSearch] = useState("");
+  const [searchKeyUp, setSearchKeyUp] = useState("");
+
+  const url = useMemo(() => {
+    const searchParam = new URLSearchParams();
+    searchParam.set("query", searchKeyUp);
+    return searchKeyUp
+      ? `${import.meta.env.VITE_MOVIE_SEARCH}${searchParam.toString()}`
+      : import.meta.env.VITE_MOVIE_LIST_ALL;
+  }, [searchKeyUp]);
+
+  const { data } = useSWR(url, fetcher);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSearchKeyUp(search);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [search, setSearchKeyUp]);
+
   return (
     <div className="bg-[#0A2647] min-h-screen w-full w-full flex justify-center py-[50px]">
       <div className="max-w-5xl w-full h-full">
-        <form
-          className="w-full h-full space-y-[30px]"
-          onSubmit={handleSubmit((data) => console.log(data))}
-        >
+        <div className="w-full h-full space-y-[30px]">
           <div className="space-y-[10px]">
             <h1 className="text-white text-7xl font-bold">Elbi Movies</h1>
             <p className="text-white text-2xl">
@@ -32,135 +40,62 @@ function App() {
 
           <div className="bg-[#2C74B3] w-full h-[2px]"></div>
 
-          <div className="flex flex-col space-y-[10px]">
-            <label htmlFor="search" className="text-white text-2xl font-bold">
-              Title :
-            </label>
+          <div className="space-y-[10px]">
             <input
               id="search"
               type="text"
-              className="bg-[#144272] text-white shadow p-[10px]"
-              {...register("title", {
-                maxLength: {
-                  value: 50,
-                  message:
-                    "The amount of search text is too long. Max Length is 50",
-                },
-              })}
+              className="bg-[#144272] w-full text-white shadow p-[16px] rounded hover:opacity-[0.8]"
+              placeholder="Search Any Movies"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
             />
-            {errors?.title?.message && (
-              <p className="text-lg text-red-600">
-                {errors?.title?.message as string}
-              </p>
-            )}
+            <p className="text-white">
+              Search by titles like:{" "}
+              <span className="font-bold">
+                Fast And Furious, Dragon Ball, One Piece
+              </span>
+            </p>
           </div>
 
-          <button
-            type="button"
-            onClick={() => setFilter((state) => !state)}
-            className="bg-[#205295] p-[16px] text-white shadow hover:opacity-[0.8] active:scale-[0.8] active:opacity-[0.5]"
-          >
-            {filter ? "Close" : "Open"} Filter Menu
-          </button>
-
-          {filter && (
-            <div className="space-y-[16px]">
-              <div className="flex flex-col space-y-[10px]">
-                <label
-                  htmlFor="search"
-                  className="text-white text-2xl font-bold"
+          {/* Content */}
+          <div className="grid grid-cols-3 grid-rows-7 gap-[48px] !mt-[50px]">
+            {Array.isArray(data?.results) &&
+              data?.results.map((candidate) => (
+                <div
+                  key={candidate.id}
+                  className="group shadow-xl bg-[#144272] rounded overflow-hidden hover:border-[#2C74B3] border-[3px] border-[#0A2647]"
                 >
-                  Language :
-                </label>
-                <input
-                  id="search"
-                  type="text"
-                  className="bg-[#144272] text-white shadow p-[10px]"
-                  {...register("language", {
-                    maxLength: {
-                      value: 50,
-                      message:
-                        "The amount of search text is too long. Max Length is 50",
-                    },
-                    shouldUnregister: true,
-                  })}
-                />
-                {errors?.language?.message && (
-                  <p className="text-lg text-red-600">
-                    {errors?.language?.message as string}
-                  </p>
-                )}
-              </div>
-
-              <div className="flex flex-col space-y-[10px]">
-                <label
-                  htmlFor="search"
-                  className="text-white text-2xl font-bold"
-                >
-                  Year :
-                </label>
-                <input
-                  id="search"
-                  type="number"
-                  className="bg-[#144272] text-white shadow p-[10px]"
-                  {...register("year", {
-                    min: {
-                      value: 1900,
-                      message: "Length must be 4",
-                    },
-                    max: {
-                      value: new Date().getFullYear(),
-                      message: "Length must be 4",
-                    },
-                    valueAsNumber: true,
-                    shouldUnregister: true,
-                  })}
-                />
-                {errors?.year?.message && (
-                  <p className="text-lg text-red-600">
-                    {errors?.year?.message as string}
-                  </p>
-                )}
-              </div>
-
-              <div className="flex flex-col space-y-[10px]">
-                <label
-                  htmlFor="search"
-                  className="text-white text-2xl font-bold"
-                >
-                  Region :
-                </label>
-                <input
-                  id="search"
-                  type="text"
-                  className="bg-[#144272] text-white shadow p-[10px]"
-                  {...register("region", {
-                    maxLength: {
-                      value: 50,
-                      message:
-                        "The amount of search text is too long. Max Length is 50",
-                    },
-                    shouldUnregister: true,
-                  })}
-                />
-              </div>
-              {errors?.region?.message && (
-                <p className="text-lg text-red-600">
-                  {errors?.region?.message as string}
-                </p>
-              )}
-            </div>
-          )}
-
-          <div className="w-full h-full">
-            <button
-              type="submit"
-              className="bg-[#205295] p-[16px] text-white shadow hover:opacity-[0.8] active:scale-[0.8] active:opacity-[0.5] w-full"
-            >
-              SEARCH
-            </button>
+                  <div className="w-full h-[500px] bg-[#0A2647] overflow-hidden">
+                    <img
+                      src={`${import.meta.env.VITE_IMAGE_URL}/${
+                        candidate.poster_path
+                      }`}
+                      alt={candidate.title}
+                      className="w-full h-full object-cover transition group-hover:scale-[1.2]"
+                    />
+                  </div>
+                  <div className="p-[16px]">
+                    <p className="text-white text-2xl">{candidate.title}</p>
+                    <p className="text-white text-md">
+                      {candidate.release_date}
+                    </p>
+                  </div>
+                </div>
+              ))}
           </div>
-        </form>
+
+          {/* Pagination */}
+          <div className="w-full h-full grid grid-cols-2 gap-[20px] justify-end">
+            {["Previous", "Next"].map((candidate) => (
+              <button
+                className="bg-[#205295] p-[16px] text-white shadow hover:opacity-[0.8] active:scale-[0.8] active:opacity-[0.5]"
+                key={candidate}
+              >
+                {candidate}
+              </button>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -3,7 +3,7 @@ export default function fetcher(url: string) {
     headers: {
       accept: "application/json",
       Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ZjIwZTI3ZmY5ZTUxMTdiY2FlNDEyZjRhNGU4MTEyMyIsInN1YiI6IjY0ZTYyNDhlNzcwNzAwMDBhZTA2ZTM0NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.TA8Z8JtMng1RYdz9wTad8RXhmKXOaI3Ltt_0KAeq_pY",
+        `Bearer ${import.meta.env.VITE_API_KEY}`,
     },
   }).then((res) => res.json());
 }
