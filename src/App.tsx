c21bf0808d4e5b739df20e87088148a84f1c57e2
ElbiGans04 <rhafaelbijaksana04@gmail.com>
2023-08-25 14:54:19 +0700

    feat: make page responsive

--- src/App.tsx
+++ src/App.tsx
@@ -36,11 +36,11 @@ function App() {
     <div
       className={`bg-[#0A2647] min-h-screen w-full w-full flex justify-center py-[50px]`}
     >
-      <div className="max-w-5xl w-full h-full">
+      <div className="xl:max-w-5xl lg:max-w-3xl px-[50px] w-full h-full">
         <div className="w-full h-full space-y-[30px]">
           <div className="space-y-[10px]">
-            <h1 className="text-white text-7xl font-bold">Elbi Movies</h1>
-            <p className="text-white text-2xl">
+            <h1 className="text-white xl:text-7xl text-3xl md:text-4xl font-bold">Elbi Movies</h1>
+            <p className="text-white xl:text-2xl md:text-xl text-lg">
               Search any movie for{" "}
               <span className="font-bold underline">free</span>
             </p>
@@ -52,12 +52,12 @@ function App() {
             <input
               id="search"
               type="text"
-              className="bg-[#144272] w-full text-white shadow p-[16px] rounded hover:opacity-[0.8]"
+              className="bg-[#144272] w-full text-white shadow p-[8px] lg:p-[16px] rounded hover:opacity-[0.8] lg:text-md text-sm"
               placeholder="Search Any Movies"
               value={search}
               onChange={(e) => setSearch(e.target.value)}
             />
-            <p className="text-white">
+            <p className="text-white lg:text-md text-sm">
               Search by titles like:{" "}
               <span className="font-bold">
                 Fast And Furious, Dragon Ball, One Piece
@@ -88,7 +88,7 @@ function App() {
 
           {/* Content */}
           {!isLoading && (
-            <div className="grid grid-cols-3 grid-rows-7 gap-[48px] !mt-[50px]">
+            <div className="grid grid-cols-1 lg:grid-cols-2  xl:grid-cols-3 grid-rows-7 gap-[48px] !mt-[50px]">
               {Array.isArray(data?.results) &&
                 data?.results.map((candidate) => (
                   <div
@@ -105,8 +105,8 @@ function App() {
                       />
                     </div>
                     <div className="p-[16px]">
-                      <p className="text-white text-2xl">{candidate.title}</p>
-                      <p className="text-white text-md">
+                      <p className="text-white text-xl xl:text-2xl">{candidate.title}</p>
+                      <p className="text-white text-sm xl:text-md">
                         {candidate.release_date}
                       </p>
                     </div>
